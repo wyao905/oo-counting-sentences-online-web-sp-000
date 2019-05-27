033ee76eeaa107dda0309d8bb237cdafaae96c92
Warren Yao <warrenyca905@gmail.com>
2019-05-27 23:12:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
