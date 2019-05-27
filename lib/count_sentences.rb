e8652c6f71c683a71e577ec54f21dddf1a3822b6
Warren Yao <warrenyca905@gmail.com>
2019-05-27 23:19:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    split_array = self.split(/[.?!]/)
+    split_array.each do |element|
+      if element == ""
+        split_array.delete(element)
+      end
+    end
+    return split_array.length
   end
 end
